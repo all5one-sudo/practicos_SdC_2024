01529caf0e9481056e2694b9f7c3080a8349d2f7
all5one-sudo <federico.villar@mi.unc.edu.ar>
2024-06-17 17:11:19 -0700

    Avances compilaciones Drvs

---
+++ practico5/FuentesDrv3/.drv3.mod.cmd
@@ -0,0 +1,1 @@
+cmd_/home/debian/Documents/practicos_SdC_2024/practico5/FuentesDrv3/drv3.mod := printf '%s\n'   drv3.o | awk '!x[$$0]++ { print("/home/debian/Documents/practicos_SdC_2024/practico5/FuentesDrv3/"$$0) }' > /home/debian/Documents/practicos_SdC_2024/practico5/FuentesDrv3/drv3.mod
